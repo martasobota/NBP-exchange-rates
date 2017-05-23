e0f4e0cf23b31f5a73f4a9a830e1a2bc4876e1d1
martazaryn <martazaryn@gmail.com>
2017-05-23 18:05:13 +0200

    Views

--- NBP/exchange_rates/views.py
+++ NBP/exchange_rates/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+# from django.http import HttpResponse
+from django.views import View
+import json
+import requests
 
-# Create your views here.
+class USD_PLN_main(APIView):
+
+	def get_object(self, pk):
+	    try:
+	    	return NBP.objects.get(pk=pk)
+		except NBP.DoesNotExist:
+		    raise Http404
+	def get(self, request, id, format=None):
+		nbp = self.get_object(id)
+		serializer = NBPSerializer(nbp, context={"request": request}) 
+		return Response(serializer.data)
+	def delete(self, request, id, format=None):
+		nbp = self.get_object(id)
+		nbp.delete()
+		return Response(status=status.HTTP_204_NO_CONTENT)
+	def put(self, request, id, format=None): 
+			nbp = self.get_object(id)
+			serializer = NBPSerializer(nbp, data=request.data) 
+			if serializer.is_valid():
+				serializer.save()
+			return Response(serializer.data)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+	# def get(self, request):
+	# 	usd_pln = 'http://api.nbp.pl/api/exchangerates/tables/B/?format=json'
+	# 	r_usd_pln = requests(usd_pln)
+	# 	details = r_usd_pln.json()
+	# 	return details
