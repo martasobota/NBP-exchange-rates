8f9ae89d43507b1c772fa8e957595f8ac4e948f4
martazaryn <martazaryn@gmail.com>
2017-05-23 18:04:21 +0200

    URLs

--- NBP/NBP/urls.py
+++ NBP/NBP/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from exchange_rates.views import USD_PLN_main
+# 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^nbp/', USD_PLN_main.as_view(), name='nbp'),
 ]
