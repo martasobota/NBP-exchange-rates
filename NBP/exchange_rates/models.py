07d16b321a236d52fa78dab3000526cf0e5bedcb
martazaryn <martazaryn@gmail.com>
2017-05-23 18:04:58 +0200

    Models draft version

--- NBP/exchange_rates/models.py
+++ NBP/exchange_rates/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+TABLES = (
+	(1, 'A'),
+	(2, 'B')
+	(3, 'C')
+	)
+
+# CURRENCIES = 
+
+class NBP(models.Model):
+	table = models.IntegerField(choices=TABLES)
+	currency = models.CharField(max_length=64)
+	code = models.CharField(max_length=4)
+	effective_date = models.DateField()
+	rate = models.FloatField(max_length=8)
+
+# class Exchange_rates(models.Model):
+# 	from_rate = models.
+# 	to_rate = models.
+	
