3d05fc540f699dd642253539bb3d8dee1d55887d
martazaryn <martazaryn@gmail.com>
2017-05-24 19:51:46 +0200

    NBP service file with date looping and changed request.get

--- NBP/exchange_rates/nbp_service.py
+++ NBP/exchange_rates/nbp_service.py
@@ -1,9 +1,20 @@
 import requests
+from datetime import date, timedelta
 
 def get_data(code):
-    url = 'http://api.nbp.pl/api/exchangerates/tables/B/?format=json' 
-    params = {'code': code}
-    r = requests.get('http://api.nbp.pl/api/exchangerates/tables/B/?format=json', params=params)
-    nbp = r.json()
-    nbp_list = {'nbp':nbp[]}
-    # nbp_list = {'nbp':nbp['results']}
+	last_week = []
+	today = date.today()
+	last_week.append("{:%Y/%m/%d}".format(today))
+
+	day = today - timedelta(days=1)
+	for d in range(7):
+		last_week.append("{:%Y/%m/%d}".format(day))
+		day -= timedelta(days=1)
+
+
+	# return requests.get('http://api.nbp.pl/api/exchangerates/rates/A/' + code + '/' + today + '/' + last_week[-1] + '/?format=json').json()
+# for rate in last_week:
+# 	print(requests.get('http://api.nbp.pl/api/exchangerates/rates/A/' + code + '/' + rate + '/?format=json').json())
+
+# return requests.get('http://api.nbp.pl/api/exchangerates/rates/A/' + code + '/' + date.now -  + '/' + today + '/?format=json').json()
+get_data('usd')
