04d7429c88147054cda5dc07aa8f368dda28490c
martazaryn <martazaryn@gmail.com>
2017-05-24 19:51:05 +0200

    View

--- NBP/exchange_rates/views.py
+++ NBP/exchange_rates/views.py
@@ -1,36 +1,24 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
 from django.views import View
 import json
 import requests
 from . import nbp_service
 from django.views import generic
 
+# class NBPPage(generic.TemplateView):
+#     def get(self,request):
+#     	code = 'usd'
+#     	# nbp_data = nbp_service.get_data(code)
+#     	nbp_data = nbp_service
+#     	return render(request,'exchange_rates/nbp.html',nbp_data)
 
-# class USD_PLN_main(APIView):
 
-# 	def get_object(self, pk):
-# 	    try:
-# 	    	return NBP.objects.get(pk=pk)
-# 		except NBP.DoesNotExist:
-# 		    raise Http404
-# 	def get(self, request, id, format=None):
-# 		nbp = self.get_object(id)
-# 		serializer = NBPSerializer(nbp, context={"request": request}) 
-# 		return Response(serializer.data)
-# 	def delete(self, request, id, format=None):
-# 		nbp = self.get_object(id)
-# 		nbp.delete()
-# 		return Response(status=status.HTTP_204_NO_CONTENT)
-# 	def put(self, request, id, format=None): 
-# 			nbp = self.get_object(id)
-# 			serializer = NBPSerializer(nbp, data=request.data) 
-# 			if serializer.is_valid():
-# 				serializer.save()
-# 			return Response(serializer.data)
-# 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class NBPPage(View):
+    def get(self,request, NBP_code):
+    	
+    	rate = NBP.objects.get(pk=NBP_code)
+    	ctx = { 'rate' : rate,
 
-class NBPPage(generic.TemplateView):
-    def get(self,request):
-        nbp_list = nbp_service.get_data('usd')
-        return render(request,'nbp.html',nbp_list)
+    	}
+    	# nbp_data = nbp_service.get_data(code)
+    	return render(request,'exchange_rates/nbp.html',nbp_data)
