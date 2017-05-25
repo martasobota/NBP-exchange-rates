a16dd7f6a614ced80d98485e2f68fe06fc4c0d6e
martazaryn <martazaryn@gmail.com>
2017-05-25 11:10:13 +0200

    NBP service continue

--- NBP/exchange_rates/nbp_service.py
+++ NBP/exchange_rates/nbp_service.py
@@ -1,20 +1,66 @@
 import requests
 from datetime import date, timedelta
+import plotly.plotly as py
+import plotly.graph_objs as go
+import plotly.figure_factory as ff
 
-def get_data(code):
-	last_week = []
-	today = date.today()
-	last_week.append("{:%Y/%m/%d}".format(today))
-
-	day = today - timedelta(days=1)
-	for d in range(7):
-		last_week.append("{:%Y/%m/%d}".format(day))
-		day -= timedelta(days=1)
-
-
-	# return requests.get('http://api.nbp.pl/api/exchangerates/rates/A/' + code + '/' + today + '/' + last_week[-1] + '/?format=json').json()
 # for rate in last_week:
 # 	print(requests.get('http://api.nbp.pl/api/exchangerates/rates/A/' + code + '/' + rate + '/?format=json').json())
 
-# return requests.get('http://api.nbp.pl/api/exchangerates/rates/A/' + code + '/' + date.now -  + '/' + today + '/?format=json').json()
-get_data('usd')
+def get_data(code):
+    today = date.today()
+    last_week = ["{:%Y-%m-%d}".format(today - timedelta(days=days_delta)) for days_delta in range(8)]	
+
+    url = requests.get('http://api.nbp.pl/api/exchangerates/rates/A/' + code + '/' + last_week[-1] + '/' + last_week[0] + '/?format=json').json()
+    return url
+
+    print("Url here: ")
+    print(url)
+    print('Items:')
+    print(url.items())
+    print('Test')
+    print(url['rates'][0]['effectiveDate'])
+    print(url['rates'][1])
+    print(url['rates'][2])
+    print(url['rates'][3])
+    print(url['rates'][4])
+
+# get_data('usd')
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # 
+
+def create_table():
+	global url
+	print(url)
+
+create_table()
+
+# import plotly.plotly as py
+# import plotly.figure_factory as ff
+
+# data_matrix = [['Country', 'Year', 'Population'],
+#                ['United States', 2000, 282200000],
+#                ['Canada', 2000, 27790000],
+#                ['United States', 2005, 295500000],
+#                ['Canada', 2005, 32310000],
+#                ['United States', 2010, 309000000],
+#                ['Canada', 2010, 34000000]]
+
+# table = ff.create_table(data_matrix)
+# py.iplot(table, filename='simple_table')
+
+# # # # # # # # # # # # # # # # # # # # # # # # 
+
+# import plotly.plotly as py
+# import plotly.graph_objs as go
+
+# import pandas as pd
+
+# df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv")
+
+# data = [go.Scatter(
+#           x=df.Date,
+#           y=df['AAPL.Close'])]
+
+# py.iplot(data)
