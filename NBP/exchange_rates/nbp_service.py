41191f570a9b715bece32fb9105eb8c61c2b64ac
martazaryn <martazaryn@gmail.com>
2017-05-25 22:19:38 +0200

    Multiple functions

--- NBP/exchange_rates/nbp_service.py
+++ NBP/exchange_rates/nbp_service.py
@@ -5,7 +5,7 @@ import plotly.plotly as py
 import plotly.graph_objs as go
 import plotly.figure_factory as ff
 
-def get_data(code):
+def get_7_working_days():
 	"""
 	Creates table with dates in proper format (ISO 8601)
 	Range = 15, to save dates with surplus, e.g. if you are checking exchange rates for last 7 days on Thursday, 
@@ -15,10 +15,14 @@ def get_data(code):
 	Value 15 is my prediction, if I would have more time I would try do it more accurate
 	"""
 	today = date.today()
-	last_days = ["{:%Y-%m-%d}".format(today - timedelta(days=days_delta)) for days_delta in range(15)]	
-	url = requests.get('http://api.nbp.pl/api/exchangerates/rates/A/' + code + '/' + last_days[-1] + '/' + last_days[0] + '/?format=json').json()
-	# return url
+	last_days = ["{:%Y-%m-%d}".format(today - timedelta(days=days_delta)) for days_delta in range(15)]
+	return last_days
 
+def get_data(code):	
+	url = requests.get('http://api.nbp.pl/api/exchangerates/rates/A/' + code + '/' + last_days[-1] + '/' + last_days[0] + '/?format=json').json()
+	return url
+
+def create_matrix(url):
 	data = []
 	for u in url['rates'][-7:]:
 		data.append(u)
@@ -42,4 +46,7 @@ def get_data(code):
 	table = ff.create_table(ex_rates_matrix)
 	py.plot(table, filename='ex_rates_7days_usd_table')
 
-get_data('usd')
+last_7 = get_7_working_days()
+get_data('usd', last_7)
+rate_data = get_data('usd')
+create_matrix(rate_data)
