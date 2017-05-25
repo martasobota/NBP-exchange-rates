44280e3f7fc30e966b52b193f3971d850c2dd6dd
martazaryn <martazaryn@gmail.com>
2017-05-25 21:53:38 +0200

    Views

--- NBP/exchange_rates/views.py
+++ NBP/exchange_rates/views.py
@@ -5,20 +5,9 @@ import requests
 from . import nbp_service
 from django.views import generic
 
-# class NBPPage(generic.TemplateView):
-#     def get(self,request):
-#     	code = 'usd'
-#     	# nbp_data = nbp_service.get_data(code)
-#     	nbp_data = nbp_service
-#     	return render(request,'exchange_rates/nbp.html',nbp_data)
-
-
-class NBPPage(View):
-    def get(self,request, NBP_code):
-    	
-    	rate = NBP.objects.get(pk=NBP_code)
-    	ctx = { 'rate' : rate,
-
-    	}
+class NBPPage(generic.TemplateView):
+    def get(self,request):
+    	code = 'usd'
     	# nbp_data = nbp_service.get_data(code)
+    	nbp_data = nbp_service
     	return render(request,'exchange_rates/nbp.html',nbp_data)
