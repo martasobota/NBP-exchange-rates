4941bb6e212d279d6bc388c0d7b488c7d1e9fbcd
martazaryn <martazaryn@gmail.com>
2017-05-25 12:36:59 +0200

    Changed counting days

--- NBP/exchange_rates/nbp_service.py
+++ NBP/exchange_rates/nbp_service.py
@@ -1,40 +1,81 @@
 import requests
 from datetime import date, timedelta
+import numpy as np
 import plotly.plotly as py
 import plotly.graph_objs as go
 import plotly.figure_factory as ff
 
-# for rate in last_week:
-# 	print(requests.get('http://api.nbp.pl/api/exchangerates/rates/A/' + code + '/' + rate + '/?format=json').json())
-
 def get_data(code):
-    today = date.today()
-    last_week = ["{:%Y-%m-%d}".format(today - timedelta(days=days_delta)) for days_delta in range(8)]	
+	"""
+	Creates table with dates in proper format (ISO 8601)
+	Range = 15, to save dates with surplus, e.g. if you are checking exchange rates for last 7 days on Thursday, 
+	you will need last 9 dates (there is weekend in between), but if you wanna check exchange rates on Monday 
+	or Tuesday you will need dates for last 11 days, because there are two weekends in between.
+	There are times during the year when there are also holidays, so range should be increased. 
+	Value 15 is my prediction, if I would have more time I would try do it more accurate
+	"""
 
-    url = requests.get('http://api.nbp.pl/api/exchangerates/rates/A/' + code + '/' + last_week[-1] + '/' + last_week[0] + '/?format=json').json()
-    return url
+	today = date.today()
+	last_days = ["{:%Y-%m-%d}".format(today - timedelta(days=days_delta)) for days_delta in range(15)]	
 
-    print("Url here: ")
-    print(url)
-    print('Items:')
-    print(url.items())
-    print('Test')
-    print(url['rates'][0]['effectiveDate'])
-    print(url['rates'][1])
-    print(url['rates'][2])
-    print(url['rates'][3])
-    print(url['rates'][4])
+	start = last_days[-1]
+	end = last_days[0]
+	days = np.busday_count(start,end)
+	print(days)
+	"""
+	url taking informations from last 7 days - it is buggy, because there are no new exchange rates
+	during the weekend, this line should be changed so it will be getting data for 7 working days
+	"""
+	
+	url = requests.get('http://api.nbp.pl/api/exchangerates/rates/A/' + code + '/' + last_days[-1] + '/' + last_days[0] + '/?format=json').json()
+	print(url)
+	# return url
 
-# get_data('usd')
+	# print("Url here: ")
+	# print(url)
+	# print('Items:')
+	# print(url.items())
+	# print('Test')
+	print('url len')
+	print(len(url['rates']))
+
+	data = []
+	for u in url['rates'][-7:]:
+		data.append(u)
+
+	print('proper dates:')
+	print(data)
+	print(len(data))
+	print(type(data))
+	for p in data:
+		print(p['effectiveDate'])
+		print(p['mid'])
+
+
+get_data('usd')
+
+	# data_matrix = [['Date', 'Rate'],
+	# []
+
+	# ]
+
+	# lista = []
+	# for effectiveDate, mid in url['rates']:
+	# 	lista.append(['effectiveDate'])
+	# 	lista.append(['mid'])
+
+	# print(lista)
+
+
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # 
 
-def create_table():
-	global url
-	print(url)
+# def create_table():
+	# get_data('usd')
+	# print(url)
 
-create_table()
+# create_table()
 
 # import plotly.plotly as py
 # import plotly.figure_factory as ff
